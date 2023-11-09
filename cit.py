6027dabd60c38d4c333b1377c4aa50dabfa03fea
berk-2008 <almazbekalmazbekberk@gmail.com>
2023-11-09 12:08:40 +0600

    irinchi versia

---
+++ cit.py
@@ -0,0 +1,13 @@
+# Бул гит учун ачылган птон файл
+
+# local git ке файлды кошуу
+
+# 1. git init  (Гит инициялизация кылуу)
+# 2. git add . (Учурдагы папкада турган баардык файлды гитке кошуу)
+# 3. git status (Файлдын гитке кошулуп же кошулбагандыгын текшеруу)
+# 4. git commit -m "Birinchi versia" (Коммит кылуу)
+# 5. git branch -M main
+# 6. git remote add origin https://github.com/abdulhatov/test.git (репозиториини кошуу)
+
+
+
